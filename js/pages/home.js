c9572b65f3ee20d1dcb61309e2e92aa24a9be71b
julijajarmale <julija.jarmale@gmail.com>
2022-04-12 15:51:58 +0300

    Layout css and git conflict prevention

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,3 +1,36 @@
 // component imports
 
 // components execution
+
+/* header: start */
+/* header: end */
+
+/* hero: start */
+/* hero: end */
+
+/* companies1: start */
+/* companies1: end */
+
+/* about us 1: start */
+/* about us 1: end */
+
+/* services: start */
+/* services: end */
+
+/* why choose us: start */
+/* why choose us: end */
+
+/* let's talk: start */
+/* let's talk: end */
+
+/* blogs: start */
+/* blogs: end */
+
+/* testimonials: start */ 
+/* testimonials: end */
+
+/* companies2: start */
+/* companies2: end */
+
+/* footer: start */
+/* footer: end */
