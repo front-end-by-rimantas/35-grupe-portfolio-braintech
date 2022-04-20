593a3635d705a719e57447169dd38b0d35e22e3a
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-04-20 16:55:05 +0300

    Validations js

--- js/components/features/features.js
+++ js/components/features/features.js
@@ -1,10 +1,68 @@
 function renderFeatures(selector, data) {
+    const mandatoryObjKeys = ['title', 'desc'];
+    const optionalObjKeys = ['icon'];
+    const allObjKeys = [...mandatoryObjKeys, ...optionalObjKeys];
+    const minKeysCount = mandatoryObjKeys.length;
+    const maxKeysCount = minKeysCount + optionalObjKeys.length;
+
+    if(typeof selector !== 'string') {
+        return [true, 'Selectorius turi buti stringas'];
+    }
+    if(typeof selector === '') {
+        return [true, 'Selectorius turi buti netuscias stringas'];
+    }
+
     const DOM = document.querySelector(selector);
+
+    if(!DOM) {
+        return [true, 'pagal Selector, nepavyko rasti elemento'];
+    }
+
+    if(!Array.isArray(data)) {
+        return [true, 'duomenys turi buti masyvo tipo'];
+    }
+
+    if(data.length === 0) {
+        return [true, 'Duomenyse turi buti bent vienas objektas'];
+    }
+
     let HTML = '';
     for (const item of data) {
+
+        if (typeof item !== 'object'
+        || item === null
+        || Array.isArray(item)) {
+        continue;
+        }
+
+        const keys = Object.keys(item);
+        if (keys.length < minKeysCount || keys.length > maxKeysCount) {
+            continue;
+        }
+
+        let correctObj = true;
+        for (const key of keys) {
+            if (!allObjKeys.includes(key)) {
+                correctObj = false;
+                break;
+            }
+        }
+        if (!correctObj) {
+            continue;
+        }
+
+        if (typeof item.title !== 'string'
+            || item.title.trim() === '') {
+            continue;
+        }
+        if (typeof item.desc !== 'string'
+            || item.desc.trim() === '') {
+            continue;
+        }   
+
         HTML += `
 
-        <div class="features">
+        <div class="col-lg-6 features">
         <img src="../../../img/why-choose-us/${item.icon}.png" class="ico"></img>
         
             <div class="ico2">
@@ -15,7 +73,13 @@ function renderFeatures(selector, data) {
         `;
     }
 
+    if(HTML === '') {
+        return [true, 'gautuose duomenyse nera teisingu duomenu'];
+    }
+
     DOM.innerHTML = HTML;
+
+    return [false, 'OK'];
     
 }
 
