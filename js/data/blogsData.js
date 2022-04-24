714b47a458285934ccd84c2a0bd9c888bcb53adb
julijajarmale <julija.jarmale@gmail.com>
2022-04-24 13:53:34 +0300

    blogsData

--- js/data/blogsData.js
+++ js/data/blogsData.js
@@ -1,10 +1,57 @@
 const blogsData=[
     {
         image: 'image1',
+        button: 'Digital Technology',
+        date: '16 November 2020',
+        user: 'admin',
+        title: 'Tech Firms Support Huawei Restriction, Balk and Cost',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
+    },
+    {
+        image: 'image2',
+        button: 'Software Development',
+        date: '20 November 2020',
+        user: 'admin',
+        title: 'Tech Products That Makes Its Easier to Stay at Home',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
+    },
+    {
+        image: 'image3',
+        button: 'Web Development',
+        date: '20 December 2020',
+        user: 'admin',
+        title: 'Open Source Job Report Show More Openings Fewer',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
+    },
+    {
+        image: 'image4',
         button: 'IT Services',
-        icon: 'icon1',
-        title: 'Email',
-        desc: 'support@rstheme.com'
+        date: '22 December 2020',
+        user: 'admin',
+        title: 'Open Source Job Report Show More Openings Fewer',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
+    },
+    {
+        image: 'image5',
+        button: 'Artifical Intelligence',
+        date: '28 December 2020',
+        user: 'admin',
+        title: 'Types of Social Proof What its Makes Them Effective',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
+    },
+    {
+        image: 'image6',
+        button: 'IT Services',
+        date: '30 December 2020',
+        user: 'admin',
+        title: 'Servo Project Joins The Linux Foundation Fold Desco',
+        desc: 'We denounce with righteous indige nation and dislike men who are so beguiled...',
+        link: 'Learn More'
     },
 ];
 export { blogsData };
