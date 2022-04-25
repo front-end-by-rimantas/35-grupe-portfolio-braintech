46c319b23246e77d0b19fda97875b1b834e1e45c
julijajarmale <julija.jarmale@gmail.com>
2022-04-25 21:11:17 +0300

    partners1

--- js/components/partners/partners.js
+++ js/components/partners/partners.js
@@ -5,7 +5,7 @@ function renderPartners(selector, data) {
         
         HTML += `<div class="row">
         <div class="partner-col col-6 col-md-4 col-lg-3 col-xl-2">
-             <a class="partner-link" href="#">
+             <a class="partner-link" href="${item.link}">
                 <img class="partner-logo" src="./img/companies/partners1/${item.logo}.png" alt="Partner Logo"
              /></a>
         </div>
