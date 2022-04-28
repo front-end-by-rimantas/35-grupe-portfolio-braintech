67ed7375d41695266f3a29b2621b27a52027f4c8
LeoGytis <leogytis@gmail.com>
2022-04-28 18:48:22 +0300

    icon numbers and animation

--- js/components/services/services.js
+++ js/components/services/services.js
@@ -1,12 +1,14 @@
 function services(selector, data) {
     const DOM = document.querySelector(selector);
     let HTML = ''; 
-    
-    for(let obj of data) {
+    let columnIndex = 0;
 
+    for(let obj of data) {
+        columnIndex += 1;
         HTML += `
         <div class="services-card">
         <i class="fa fa-${obj.icon} icon" aria-hidden="true" style="color:${obj.colorFront}"></i>
+        <h1 class="services-number">0${columnIndex}</h1>
         <a class="a" href="#">${obj.title}</a>
         <p class="paragraph">${obj.desc}</p>
         </div>
