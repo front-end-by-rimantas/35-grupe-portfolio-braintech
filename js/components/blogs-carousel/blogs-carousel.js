8d33f395fdb2e3046c4faf2a2c20d6b98cf73596
julijajarmale <julija.jarmale@gmail.com>
2022-04-28 20:36:14 +0300

    blogs JS before js listener

--- js/components/blogs-carousel/blogs-carousel.js
+++ js/components/blogs-carousel/blogs-carousel.js
@@ -30,6 +30,8 @@ class BlogCarousel {
         this.updateDefaultSettings();
 
         this.render();
+        this.action();
+
     }
 
     isValidSelector() {
@@ -97,24 +99,12 @@ class BlogCarousel {
     ];
 
    
-    console.log(list);
+    console.log(this);
 
       for (const item of this.data.list) {
-        HTML += `<div class="blog-item">
-        <div class="blog-style">
-          <img src="./img/blogs/1.jpg" alt="blog-image" class="blog-image"> 
-          <a href="#" class="link btn blue-btn blog-btn">Digital Technology</a>
-        </div>
-        <div class="blog-content">
-          <ul class="blog-data">
-             <li class="date"><i class="fa fa-calendar-check-o"></i>16 November 2020</li>
-             <li class="user"><i class="fa fa-user-o"></i>admin</li>
-         </ul>
-          <a class="blog-title">Tech Firms Support Huawei Restriction, Balk and Cost</a>
-          <p class="blog-p">We denounce with righteous indige nation and dislike men who are so beguiled...</p>
-          <a class="link blog-info" href="#">Learn More</a>
-        </div>
-        </div>
+        const card = new this.blogitemClass(this.data.imageFolder, item);
+        
+        HTML += `<div class="blog-item">${card.render()}</div>
         `;
     }
 
@@ -131,6 +121,10 @@ class BlogCarousel {
          
         this.blogcarouselDOM.innerHTML = HTML;
        }
+
+    action() {
+    
+      }
 }
 
 export { BlogCarousel }  
